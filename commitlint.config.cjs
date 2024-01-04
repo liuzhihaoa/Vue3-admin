5493575734aff9782472202ce0e3048fa5314b91
liuzhihao <liuzhihao@hatech.com.cn>
2024-01-04 11:43:59 +0800

    init project

---
+++ commitlint.config.cjs
@@ -0,0 +1,11 @@
+/*
+ * @Author: liuzhihao
+ * @email: liuzhihao@hatech.com.cn
+ * @Date: 2024-01-04 11:27:32
+ * @LastEditors: liuzhihao
+ * @LastEditTime: 2024-01-04 11:27:42
+ * @description: 描述
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"]
+};
